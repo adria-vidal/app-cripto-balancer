3d494dd70494df97e903fdf6c5fc63b64b06be0e
adriavidal.develop <adriavidal.develop@gmail.com>
2024-06-02 00:34:12 +0000

    navbar y footer 50%

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,15 @@ import { AddCryptoComponent } from "./add-crypto/add-crypto.component";
 import { TotalBalanceComponent } from "./total-balance/total-balance.component";
 import { TransactionHistoryComponent } from "./transaction-history/transaction-history.component";
 import { LoginComponent } from "./components/login/login.component";
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
     standalone: true,
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    imports: [RouterModule, CommonModule, AddCryptoComponent, TotalBalanceComponent, TransactionHistoryComponent, LoginComponent]
+    imports: [RouterModule, CommonModule, AddCryptoComponent, TotalBalanceComponent, TransactionHistoryComponent, LoginComponent, NavComponent, FooterComponent]
 })
 export class AppComponent {
   authService = inject(AuthService);
---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  standalone: true,
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss'],
+  imports: [CommonModule, RouterModule]
+})
+export class NavComponent { }
